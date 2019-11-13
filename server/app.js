127aaa894681fa8b26f36cf80c8b0b40389e1428
Daryl Marco <darmarco@gmail.com>
2019-11-12 21:25:15 -0800

    Change /products proxy endpoint to AWS ip address

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const PORT = 5001;
 
 app.use(proxy('/api/seller', { target: 'http://localhost:5000' }));
 app.use(proxy('/api/checkout', { target: 'http://localhost:1234' }));
-app.use(proxy('/products', { target: 'http://localhost:3333' }));
+app.use(proxy('/products', { target: 'http://3.18.101.198:3333' }));
 
 
 app.use(express.static('./public'));
